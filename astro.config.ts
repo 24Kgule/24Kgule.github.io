742ae7a93495c12023e53af03428b3b3847cb206
Yuishin <24Kgule@gmail.com>
2026-08-18 13:10:38 +0800

    bugfix: change output method

--- astro.config.ts
+++ astro.config.ts
@@ -40,11 +40,12 @@ export default defineConfig({
 
   // [Adapter]
   // https://docs.astro.build/en/guides/deploy/
-  adapter: vercel({ imageService: true }),
-  output: 'server',
+  // adapter: vercel({ imageService: true }),
+  // output: 'server',
   // Local (standalone)
   // adapter: node({ mode: 'standalone' }),
   // output: 'server',
+  output: 'static',
 
   // [Assets]
   image: {
